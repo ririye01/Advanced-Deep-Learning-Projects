4be078593af509eacf87c1414dc63337a7381447
kassi-bertrand <bertran.billy@gmail.com>
2024-02-03 13:08:31 -0600

    TODO: hf-dataset/entrypoint.sh - Launch the pipeline

--- hf-dataset/entrypoint.sh
+++ hf-dataset/entrypoint.sh
@@ -3,3 +3,5 @@
 apt-get -qq update && apt-get -qq install --no-install-recommends -y libgl1-mesa-glx libglib2.0-0
 
 pip install -r ./requirements.txt
+
+# TODO: Launch the distributed pipeline using accelerate
