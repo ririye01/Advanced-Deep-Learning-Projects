d52310d954bdeb74f1acbb2d83f24fb50d718ed4
kassi-bertrand <bertran.billy@gmail.com>
2024-02-03 15:23:03 -0600

    Added line to launch script

--- hf-dataset/entrypoint.sh
+++ hf-dataset/entrypoint.sh
@@ -5,3 +5,4 @@ apt-get -qq update && apt-get -qq install --no-install-recommends -y libgl1-mesa
 pip install -r ./requirements.txt
 
 # TODO: Launch the distributed pipeline using accelerate
+accelerate launch --num_processes=2 stable-diffusion-pipeline.py "runwayml/stable-diffusion-v1-5" prompts.txt -n 4 -o /output/ negative_prompt.txt
