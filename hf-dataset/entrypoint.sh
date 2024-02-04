01240355d8031b4f20499484f1932b7fec3b1388
kassi-bertrand <bertran.billy@gmail.com>
2024-02-04 00:16:55 -0600

    Updated entrypoint.sh

--- hf-dataset/entrypoint.sh
+++ hf-dataset/entrypoint.sh
@@ -5,3 +5,7 @@ apt-get -qq update && apt-get -qq install --no-install-recommends -y libgl1-mesa
 pip install -r ./requirements.txt
 
 accelerate launch --num_processes=4 stable-diffusion-pipeline.py "runwayml/stable-diffusion-v1-5" prompts.txt -n 256 -o /output/ negative_prompt.txt
+
+# python update_metadata_using_clip.py /output/temp.jsonl /output/
+
+# python to_hf.py
