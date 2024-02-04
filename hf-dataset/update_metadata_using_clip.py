911c75d6fb9f556b682533093e8faf2434b98746
kassi-bertrand <bertran.billy@gmail.com>
2024-02-04 15:04:02 -0600

    Implemented basic checkpointing

--- hf-dataset/update_metadata_using_clip.py
+++ hf-dataset/update_metadata_using_clip.py
@@ -1,8 +1,37 @@
 import json
+import logging
 import argparse
 import os
 from PIL import Image
 from clip_interrogator import Config, Interrogator
+
+
+# ----------------------------LOGGING SETUP------------------------------------
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+main_logger = logging.getLogger(__name__)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+
+main_logger = logging.getLogger(__name__)
+main_logger.setLevel(logging.INFO)
+main_file_handler = logging.FileHandler("clip-interrogation.log")
+main_file_handler.setFormatter(formatter)
+main_logger.addHandler(main_file_handler)
+
+# -----------------------------------------------------------------------------
+
+# ----------------------CLIP INTERROGATION SETUP-------------------------------
+caption_model_name = 'blip-large'
+clip_model_name = 'ViT-L-14/openai'
+config = Config()
+config.clip_model_name = clip_model_name
+config.caption_model_name = caption_model_name
+ci = Interrogator(config)
+# -----------------------------------------------------------------------------
 
 # Function to interrogate CLIP and update metadata
 def interrogate_and_update_metadata(line, output_dir):
@@ -22,18 +51,6 @@ def interrogate_and_update_metadata(line, output_dir):
     metadata_entry["interrogation"] = clip_interrogation
 
     return metadata_entry
-
-# Input and output file paths
-temp_jsonl_path = "temp.jsonl"
-output_jsonl_path = "metadata.jsonl"
-
-# Importing CLIP interrogation functions
-caption_model_name = 'blip-large'
-clip_model_name = 'ViT-L-14/openai'
-config = Config()
-config.clip_model_name = clip_model_name
-config.caption_model_name = caption_model_name
-ci = Interrogator(config)
 
 def image_to_prompt(image, mode):
     ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
@@ -64,11 +81,40 @@ def main():
     OUTPUT_DIR = args.output_dir
 
     # Open temp.jsonl for reading and metadata.jsonl for writing
-    with open(TEMP_JSONL_PATH, 'r') as temp_file, open(os.path.join(OUTPUT_DIR, "metadata.jsonl"), 'w') as output_file:
-        for line in temp_file:
+    metadata_file_path = os.path.join(OUTPUT_DIR, "metadata.jsonl")
+    mode = 'a' if os.path.exists(metadata_file_path) else 'w'
+    
+    # Load the processed line number from a file, or start from the beginning
+    last_processed_line_file = os.path.join(OUTPUT_DIR, "last_processed_line.txt")
+    start_line = 1
+
+    if os.path.exists(last_processed_line_file):
+        with open(last_processed_line_file, 'r') as last_processed_file:
+            content = last_processed_file.read().strip()
+            if content:
+                try:
+                    start_line = int(content) + 1
+                except ValueError:
+                    main_logger.warning(f"Error: Invalid content in last_processed_line.txt. Starting from the beginning.")
+                    start_line = 1
+
+    with open(TEMP_JSONL_PATH, 'r') as temp_file, open(metadata_file_path, mode) as output_file:     
+
+        for i, line in enumerate(temp_file, start=1):
+            if i < start_line:
+                main_logger.info(f"Skipping line {i} in temp.jsonl.")
+                continue
+
             # Process each line and write updated metadata to metadata.jsonl
             updated_metadata_entry = interrogate_and_update_metadata(line, OUTPUT_DIR)
             output_file.write(json.dumps(updated_metadata_entry) + '\n')
+            main_logger.info(f"Successfully generated CLIP interrogation for image {i}")
+
+            # Update the last processed line number after writing each line
+            with open(last_processed_line_file, 'w') as last_processed_file:
+                last_processed_file.write(str(i))
+            
+            main_logger.info(f"'last_processed_line.txt' was updated. The last processed line is now {i}.")
 
     # Remove temp.jsonl file
     # os.remove(TEMP_JSONL_PATH)
