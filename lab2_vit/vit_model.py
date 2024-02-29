23249752baf6a9e619529c1bcb267d8df9293fb4
kassi-bertrand <bertran.billy@gmail.com>
2024-02-28 21:39:03 -0600

    Implemented ViT self attention network module

--- lab2_vit/vit_model.py
+++ lab2_vit/vit_model.py
@@ -2,8 +2,10 @@ import numpy as np
 
 from tqdm import tqdm, trange
 
+import math
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from dataclasses import dataclass
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
@@ -54,17 +56,56 @@ class VITConfig:
 # NOTE: Kassi is implementing VIT Multi head attention module
 class MultiHeadSelfAttention(nn.Module):
     def __init__(self, config: VITConfig):
-        super().__init__()
+        super(MultiHeadSelfAttention).__init__()
         assert config.n_embed % config.n_head == 0, f"Can't divide dimension {config.n_embed} into {config.n_head} heads"
 
+        # I am using this linear layer to initialize K, Q, V matrices for ALL heads in ONE go, instead of using a for-loop. More efficient.
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed, bias=config.bias)
+
+        # This linear layer will produce the output of this module
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed, bias=config.bias)
+        
         self.n_head = config.n_head
         self.n_embed = config.n_embed
         self.dropout = config.dropout
-        # TODO: Other properties...
+        
+        # SUGGESTION: Use Flash Attention? PyTorch >= 2.0 has it built-in. Would be nice if we want to go BRRRRR 🚀
+        self.flash = None
 
     def forward(self, sequences):
-        # TODO: Implement forward pass
-        pass
+        B, T, C = sequences.size() # batch size, sequence length, embedding dimensionality (n_embed)
+
+        # Generate the Q, K, V matrices in ONE go... THEN separate them in three matrices 🙂
+        # TORCH SPLIT REFERENCE: https://pytorch.org/docs/stable/generated/torch.split.html
+        q, k, v = self.c_attn(sequences).split(self.n_embed, dim=2)
+
+        # NOTE: Instead of using a for-loop, like in the blog we can introduce a "head" dimension. Why? To process all the heads in parallel
+        # REFERENCE: Learned that from Andrej's NanoGPT. It's pretty smart 🧠
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # self-attention (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if self.flash:
+            # TODO: Flash Attention line would come here
+            pass
+        else:
+            # Manual implementation of attention.
+            # 1 - Dot product all the queries with the keys. NOTE: It does it for all the heads in ONE go.
+            attn_scr = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+
+            # 2 - Softmax so that the attention scores get mapped between 0 and 1
+            attn_scr = F.softmax(attn_scr, dim=-1)
+
+            # 3 - Compute weighted sum of 'v' vectors using attention scores
+            y = attn_scr @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        
+        # output projection
+        y = self.c_proj(y)
+
+        return y
 
 # Trevor
 class ViTBlock(nn.Module):
