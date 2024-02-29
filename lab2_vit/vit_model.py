2dac7c1950dbd9e455aaf03028f53747629ca4ee
TrevorDohm <tdohm@smu.edu>
2024-02-29 17:14:24 -0600

    Model In Work (Added Module, Block)

--- lab2_vit/vit_model.py
+++ lab2_vit/vit_model.py
@@ -1,7 +1,6 @@
+# External Imports
 import numpy as np
-
 from tqdm import tqdm, trange
-
 import math
 import torch
 import torch.nn as nn
@@ -10,36 +9,14 @@ from dataclasses import dataclass
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
-
 from torchvision.transforms import ToTensor
-from torchvision.datasets.mnist import MNIST
+from torchvision import transforms, datasets
 
-# -----------------------------------------------------------------------------
-# Utility functions. Would be nice to move them in another file called 'utils.py' or something...
-def patchify(images, n_patches):
-    n, c, h, w = images.shape
+# Seeding (Not Needed)
+# np.random.seed(0)
+# torch.manual_seed(0)
 
-    assert h == w, "Patchify method is implemented for square images only"
-
-    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
-    patch_size = h // n_patches
-
-    for idx, image in enumerate(images):
-        for i in range(n_patches):
-            for j in range(n_patches):
-                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
-                patches[idx, i * n_patches + j] = patch.flatten()
-    return patches
-
-def get_positional_embeddings(sequence_length, d):
-    result = torch.ones(sequence_length, d)
-    for i in range(sequence_length):
-        for j in range(d):
-            result[i][j] = np.sin(i / (10000 ** (j / d))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / d)))
-    return result
-# -----------------------------------------------------------------------------
-
-# ----------------------------- VIT CONFIG ------------------------------------
+# Vit Config Dataclass
 @dataclass
 class VITConfig:
     block_size: int = None # TODO: Choose block_size
@@ -49,11 +26,36 @@ class VITConfig:
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     # TODO: Add other configs ? 🤔
 
-# -----------------------------------------------------------------------------
-    
-# ----------------------------- VIT MODEL -------------------------------------
-    
-# NOTE: Kassi is implementing VIT Multi head attention module
+# Utility Functions: Patchify, Positional Embeddings
+
+# Patchify: Divide Images Into Patches
+def patchify(images, n_patches):
+
+    # Obtain Image Shape
+    n, c, h, w = images.shape
+    assert h == w, "Patchify Method Implemented For Square Images Only"
+
+    # Initialize Patches With Zeros, Compute Patch Size
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    # Iterate Over Images, Compute Patches
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+                patches[idx, i * n_patches + j] = patch.flatten()
+    return patches
+
+# Positional Embeddings: Generate Positional Embeddings For Transformer
+def get_positional_embeddings(sequence_length, d):
+    result = torch.ones(sequence_length, d)
+    for i in range(sequence_length):
+        for j in range(d):
+            result[i][j] = np.sin(i / (10000 ** (j / d))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / d)))
+    return result
+
+# Simple Multi-Head Self-Attention Module
 class MultiHeadSelfAttention(nn.Module):
     def __init__(self, config: VITConfig):
         super(MultiHeadSelfAttention).__init__()
@@ -107,10 +109,166 @@ class MultiHeadSelfAttention(nn.Module):
 
         return y
 
-# Trevor
+# ViT Block Defining Architecture
 class ViTBlock(nn.Module):
-    pass
+    def __init__(self, hidden_d, n_heads, mlp_ratio = 4):
+        super(ViTBlock, self).__init__()
+
+        # Hidden Dim, No. Heads
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        # Norm, Attention, Norm, MLP
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.attn = MultiHeadSelfAttention(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
     
-# Trevor
+    # Attention, Then Linear Layer
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        out = out + self.mlp(self.norm2(x))
+        return out
+    
+# ViT Model (Pass Images Through Transformer Encoder Blocks, Then Classification MLP)
 class ViT(nn.Module):
-    pass
+    def __init__(self, chw, n_patches = 7, n_blocks = 2, hidden_d = 8, n_heads = 2, out_d = 10):
+        super(ViT, self).__init__()
+
+         # Attributes
+        self.chw = chw  # ( C Channels, H Height, W Width )
+        self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+
+        # Input, Patch Sizes
+        assert (
+            chw[1] % n_patches == 0
+        ), "Input Shape Not Entirely Divisible By No. Patches"
+        assert (
+            chw[2] % n_patches == 0
+        ), "Input Shape Not Entirely Divisible By No. Patches"
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)
+
+        # Linear Mapper (Embedding)
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+
+        # Learnable Classification Token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+
+        # Positional Embedding
+        self.register_buffer(
+            "positional_embeddings",
+            get_positional_embeddings(n_patches**2 + 1, hidden_d),
+            persistent = False,
+        )
+
+        # Transformer Encoder Blocks
+        self.blocks = nn.ModuleList(
+            [ViTBlock(hidden_d, n_heads) for _ in range(n_blocks)]
+        )
+
+        # Classification MLP
+        self.mlp = nn.Sequential(nn.Linear(self.hidden_d, out_d), nn.Softmax(dim = -1))
+
+    def forward(self, images):
+
+        # Dividing Images Into Patches
+        n, c, h, w = images.shape
+        patches = patchify(images, self.n_patches).to(self.positional_embeddings.device)
+
+        # Running Linear Layer Tokenization
+        # Map Vector Corresponding To Each Patch To Hidden Size Dim
+        tokens = self.linear_mapper(patches)
+
+        # Adding Class Token
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim = 1)
+
+        # Adding Positional Embedding
+        out = tokens + self.positional_embeddings.repeat(n, 1, 1)
+
+        # Transformer Blocks
+        for block in self.blocks:
+            out = block(out)
+
+        # Getting Class Token
+        out = out[:, 0]
+
+        # Map To Output Dim, Output Category Distribution
+        return self.mlp(out)
+    
+# Training
+def main():
+
+    # Transform Initialization
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
+    ])
+
+    # Load Data, Transform
+    train_set = datasets.ImageFolder(root = "../data_imagenet/train", transform = transform)
+    test_set = datasets.ImageFolder(root = "../data_imagenet/val", transform = transform)
+    train_loader = DataLoader(train_set, shuffle = True, batch_size = 128)
+    test_loader = DataLoader(test_set, shuffle = False, batch_size = 128)
+
+    # Defining model and training options
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(
+        "Using device: ",
+        device,
+        f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "",
+    )
+    model = ViT(
+        (1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10
+    ).to(device)
+    N_EPOCHS = 5
+    LR = 0.005
+
+    # Training loop
+    optimizer = Adam(model.parameters(), lr=LR)
+    criterion = CrossEntropyLoss()
+    for epoch in trange(N_EPOCHS, desc="Training"):
+        train_loss = 0.0
+        for batch in tqdm(
+            train_loader, desc=f"Epoch {epoch + 1} in training", leave=False
+        ):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+
+            train_loss += loss.detach().cpu().item() / len(train_loader)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
+
+    # Test loop
+    with torch.no_grad():
+        correct, total = 0, 0
+        test_loss = 0.0
+        for batch in tqdm(test_loader, desc="Testing"):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            test_loss += loss.detach().cpu().item() / len(test_loader)
+
+            correct += torch.sum(torch.argmax(y_hat, dim=1) == y).detach().cpu().item()
+            total += len(x)
+        print(f"Test loss: {test_loss:.2f}")
+        print(f"Test accuracy: {correct / total * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    main()
